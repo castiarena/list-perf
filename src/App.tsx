d1a36275b88c986810f2f44d275453903e40e230
interview <interview@Alexandrs-MacBook-Pro.local>
2022-08-24 22:17:17 +0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+const Home = ({}) => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Counter />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <span>
+        <span>Links </span>
+        <Link className="App-link" to="/test-data">
+          Test data
+        </Link>
+        <span> </span>
+        <Link className="App-link" to="/performance">
+          Performance measurements
+        </Link>
+      </span>
+    </header>
+  );
+};
+
+const TestData = ({}) => {
+  return <div>test data</div>;
+};
+
+const PerfMeasurements = ({}) => {
+  return <div>perf measurments</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="test-data" element={<TestData />} />
+        <Route path="performance" element={<PerfMeasurements />} />
+      </Routes>
     </div>
   );
 }
