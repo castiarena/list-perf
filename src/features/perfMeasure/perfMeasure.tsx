17565aae2d11978618a3b78b476506babd2df23d
interview <interview@Alexandrs-MacBook-Pro.local>
2022-08-26 22:14:02 +0300

    add stats

--- src/features/perfMeasure/perfMeasure.tsx
+++ src/features/perfMeasure/perfMeasure.tsx
@@ -1,16 +1,30 @@
 import { useSelector } from "react-redux";
+import stats from "stats-lite";
 import { Measure } from "./interfaces";
 import { selectPerfMeasure } from "./perfMeasureSlice";
+
+function floorMs(value: Number): Number {
+  return Math.floor(Number(value) * 100) / 100;
+}
 
 const OneMeasure = ({ measure }: { measure: Measure }) => (
   <div className="perf-measures-container">
     <span>{`Phase: ${measure.phase}`}</span>{" "}
-    <span>
-      {`Actual: ${Math.floor(Number(measure.actual) * 100) / 100}ms`}{" "}
-    </span>{" "}
-    <span>{`Base: ${Math.floor(Number(measure.base) * 100) / 100}ms`}</span>{" "}
+    <span>{`Actual: ${floorMs(Number(measure.actual))}ms`} </span>{" "}
+    <span>{`Base: ${floorMs(Number(measure.base))}ms`}</span>{" "}
   </div>
 );
+
+const Stats = ({ data }: { data: Measure[] }) => {
+  const actuals = data.map((d) => Number(d.actual));
+  return (
+    <div className="perf-measures-container">
+      <span>{`Mean: ${floorMs(stats.mean(actuals))}`}</span>{" "}
+      <span>{`Median: ${floorMs(stats.median(actuals))}`} </span>{" "}
+      <span>{`90%: ${floorMs(stats.percentile(actuals, 0.9))}`}</span>{" "}
+    </div>
+  );
+};
 
 export const PerfMeasure = () => {
   const data = useSelector(selectPerfMeasure);
@@ -22,11 +36,17 @@ export const PerfMeasure = () => {
           <OneMeasure key={i} measure={m} />
         ))}
       </div>
+      <div className="text-container stats-container">
+        <Stats data={data.reactWindow} />
+      </div>
       <div className="text-label">Plain list:</div>
       <div className="text-container">
         {data.plainList.map((m, i) => (
           <OneMeasure key={i} measure={m} />
         ))}
+      </div>
+      <div className="text-container stats-container">
+        <Stats data={data.plainList} />
       </div>
     </div>
   );
